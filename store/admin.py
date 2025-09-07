c2e093bfe7a219291501f4d0c0065d8d70e76902
Narton44 <sosokom5522@gmail.com>
2025-09-08 01:41:16 +0300

    fix admin

--- store/admin.py
+++ store/admin.py
@@ -4,7 +4,6 @@ from .models import (
     Product, 
     Bank,
     Buyer,
-    MeasureUnit,
     OwnCompany,
     StockIn,
     StockOut,
@@ -14,19 +13,18 @@ from .models import (
 
 @admin.register(Stock)
 class StockAdmin(admin.ModelAdmin):
-    list_display = ['name','adress',]
-    list_filter = ['name','adress',]
-    search_fields = ['name','adress',]
+    list_display = ['name', 'owner', 'adress',]
+    list_filter = ['name', 'owner', 'adress',]
+    search_fields = ['name', 'owner', 'adress',]
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name','stock','id','price',]
-    list_filter = ['name','stock','id','price',]
-    search_fields = ['name','stock','id','price',]
+    list_display = ['name','stock','id', 'quantity', 'measure_unit' 'price',]
+    list_filter = ['name','stock','id', 'quantity', 'measure_unit' 'price',]
+    search_fields = ['name','stock','id', 'quantity', 'measure_unit' 'price',]
 
 admin.site.register(Bank)
 admin.site.register(Buyer)
-admin.site.register(MeasureUnit)
 admin.site.register(OwnCompany)
 admin.site.register(StockIn)
 admin.site.register(StockOut)
